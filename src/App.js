32bc4cd02353e09cb1249f755d70941b614f4978
hugolpa <contatohugoa@gmail.com>
2023-05-19 11:39:56 -0300

    comentários
    alteração em comentários, style, adição de certificados

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
 import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './Resume.css'; // Importe o arquivo CSS para estilização adicional, conforme necessário
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faCodepen, faGithub, faReddit } from '@fortawesome/free-brands-svg-icons';
+import CertificateList from './componets/certificados';
 
+// Componente principal do currículo
 const Resume = () => {
-  const [language, setLanguage] = useState('portuguese');
+  const [language, setLanguage] = useState('portuguese'); // Define o estado 'language' e a função 'setLanguage' para controlar o idioma
 
+  // Função para alternar entre os idiomas
   const toggleLanguage = () => {
     setLanguage(prevLanguage => prevLanguage === 'portuguese' ? 'english' : 'portuguese');
   };
 
+  // Renderização do componente
   return (
     <div className="resume">
-       <div className="background">
+      <div className="background">
         <div className="shape circle"></div>
         <div className="shape trapezoid"></div>
         <div className="shape rhombus"></div>
       </div>
       <Container>
-        
 
-        <div className="text-center mb-3">
+        {/* Botão para alternar entre os idiomas */}
+        <div className="text-center mb-3 column">
           <button className="btn btn-primary" onClick={toggleLanguage}>
             {language === 'portuguese' ? 'Switch to English' : 'Alterar para Português'}
           </button>
         </div>
 
         <Row>
+          {/* Coluna para exibir a foto de perfil */}
           <Col md={4} className="text-center column">
-            <div className="profile-picture ">
+            <div className="profile-picture skills ">
               <img
                 src="https://media.licdn.com/dms/image/C4E03AQFtRkm3ypVCpA/profile-displayphoto-shrink_800_800/0/1651606830902?e=1689206400&v=beta&t=gFSgqzv3vg0RuUcrziN3oh2xL68Q2ItheeggTFpBN5I"
                 alt="Profile"
-                className="img-fluid rounded-circle"
+                className="img-fluid rounded-circle column"
               />
             </div>
           </Col>
 
-          <Col className="col gradient-bg column " md={8}>
+          {/* Coluna para exibir detalhes pessoais e educacionais */}
+          <Col className="col  column skills" md={8}>
             <div className="personal-details column">
               <h2>{language === 'portuguese' ? 'Dados Pessoais' : 'Personal Details'}</h2>
               <p>
@@ -45,7 +53,6 @@ const Resume = () => {
               <p>
                 <strong>{language === 'portuguese' ? 'E-mail:' : 'Email:'}</strong> hugo.leonardo.jobs@gmail.com
               </p>
-              
             </div>
 
             <div className="education column">
@@ -62,41 +69,71 @@ const Resume = () => {
             </div>
           </Col>
         </Row>
-
-        <div className="experience column">
-          <h2>{language === 'portuguese' ? 'Experiência' : 'Experience'}</h2>
-          <p>
-            <strong>{language === 'portuguese' ? 'Programador Júnior' : 'Junior Programmer'}</strong>
-          </p>
-          <p>
-            <strong>{language === 'portuguese' ? 'Empresa:' : 'Company:'}</strong> INOVA Comunicação inteligentes
-          </p>
-          <p>
-            <strong>{language === 'portuguese' ? 'Período:' : 'Period:'}</strong> Abril 2018 - Presente
-          </p>
-          <ul>
-            <li>{language === 'portuguese' ? 'Desenvolvimento e manutenção de aplicativos usando React.js' : 'Developing and maintaining applications using React.js'}</li>
-            <li>{language === 'portuguese' ? 'Colaboração com a equipe de desenvolvimento no ciclo de vida do projeto' : 'Collaborating with the development team throughout the project lifecycle'}</li>
-            <li>{language === 'portuguese' ? 'Resolução de problemas e depuração de código' : 'Troubleshooting and debugging code'}</li>
-          </ul>
-        </div>
-        <div className="skills column">
-          <h2>{language === 'portuguese' ? 'Habilidades Técnicas' : 'Technical Skills'}</h2>
-          <Row>
-            <Col className="col gradient-bg" md={6}>
+        <Row>
+          {/* Coluna para exibir experiência profissional */}
+          <Col className="col " md={5}>
+            <div className="experience column skills">
+              <h2>{language === 'portuguese' ? 'Experiência' : 'Experience'}</h2>
+              <p>
+                <strong>{language === 'portuguese' ? 'Programador Front End - Pleno' : 'Full - Front End Programmer'}</strong>
+              </p>
+              <p>
+                <strong>{language === 'portuguese' ? 'Empresa:' : 'Company:'}</strong> INOVA Comunicação inteligentes
+              </p>
+              <p>
+                <strong>{language === 'portuguese' ? 'Período:' : 'Period:'}</strong> Abril 2018 - Presente
+              </p>
               <ul>
-                <li>React.js</li>
-                <li>HTML</li>
-                <li>CSS</li>
+                <li>{language === 'portuguese' ? 'Desenvolvimento e manutenção de aplicativos usando React.js' : 'Developing and maintaining applications using React.js'}</li>
+                <li>{language === 'portuguese' ? 'Colaboração com a equipe de desenvolvimento no ciclo de vida do projeto' : 'Collaborating with the development team throughout the project lifecycle'}</li>
+                <li>{language === 'portuguese' ? 'Resolução de problemas e depuração de código' : 'Troubleshooting and debugging code'}</li>
               </ul>
-            </Col>
-            <Col md={6}>
-              <ul>
-                <li>JavaScript</li>
-                <li>GitHub</li>
-              </ul>
-            </Col>
-          </Row>
+            </div>
+          </Col>
+          <Col md={7}>
+            <div className="skills column">
+              <h2>{language === 'portuguese' ? 'Habilidades Técnicas' : 'Technical Skills'}</h2>
+              <Row>
+                <Col className="col " md={6}>
+                  <ul>
+                    <li>{language === 'portuguese' ? 'Linguagens: JavaScript, HTML, CSS' : 'Languages: JavaScript, HTML, CSS'}</li>
+                    <li>{language === 'portuguese' ? 'Frameworks e Bibliotecas: React.js, Redux, Axios, React Router' : 'Frameworks and Libraries: React.js, Redux, Axios, React Router'}</li>
+                    <li>{language === 'portuguese' ? 'Ferramentas de Desenvolvimento: Visual Studio Code, Git, Webpack' : 'Development Tools: Visual Studio Code, Git, Webpack'}</li>
+                    <li>{language === 'portuguese' ? 'Práticas de Desenvolvimento: Design Responsivo, Testes Unitários, Versionamento de Código (Git), Metodologias Ágeis (Scrum)' : 'Development Practices: Responsive Design, Unit Testing, Code Versioning (Git), Agile Methodologies (Scrum)'}</li>
+                  </ul>
+                </Col>
+                <Col md={6}>
+                  <ul>
+                    <li>{language === 'portuguese' ? 'Outras Habilidades: Resolução de Problemas, Depuração de Código, Colaboração em Equipe' : 'Other Skills: Troubleshooting, Code Debugging, Team Collaboration'}</li>
+                  </ul>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+        <CertificateList/>
+        <div class="d-flex justify-content-center">
+          {/* Ícones de mídia social */}
+          <div class="m-2 column">
+            <a href="https://www.linkedin.com/in/hugo-leonardo-p-a-495a40233/" target="_blank">
+              <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: '40px' }} />
+            </a>
+          </div>
+          <div class="m-2 column">
+            <a href="https://codepen.io/huguinho" target="_blank">
+              <FontAwesomeIcon icon={faCodepen} style={{ fontSize: '40px' }} />
+            </a>
+          </div>
+          <div class="m-2 column">
+            <a href="https://github.com/hugolpa/medical_record" target="_blank">
+              <FontAwesomeIcon icon={faGithub} style={{ fontSize: '40px' }} />
+            </a>
+          </div>
+          <div class="m-2 column">
+            <a href="https://www.reddit.com/user/huguinho_dev/" target="_blank">
+              <FontAwesomeIcon icon={faReddit} style={{ fontSize: '40px' }} />
+            </a>
+          </div>
         </div>
       </Container>
     </div>
---
+++ src/componets/certificados/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+const CertificateList = () => {
+  const certificates = [
+    {
+      title: 'Front-End Development Libraries',
+      imageUrl: 'https://media.licdn.com/dms/image/C5622AQHnZverM2CEEA/feedshare-shrink_1280/0/1651588898900?e=1687392000&v=beta&t=mZVWHbYBctqaAPlyct5xeALdxPsntCrbEmWZIWhIwUM',
+      link: 'https://www.freecodecamp.org/certification/fccd9852319-c758-4155-a2ef-a8ffb02a5519/front-end-development-libraries',
+    },
+    {
+      title: 'Responsive Web Design',
+      imageUrl: 'https://media.licdn.com/dms/image/C4D22AQFElWBRGA_FBQ/feedshare-shrink_1280/0/1650474249816?e=1687392000&v=beta&t=L095wew03t9a2Op4jSXBr_SXLteZh0ucS04aNBO3vRM',
+      link: 'https://www.freecodecamp.org/certification/fccd9852319-c758-4155-a2ef-a8ffb02a5519/responsive-web-design',
+    },
+    {
+      title: 'Javascript Algorithms and Data Structures',
+      imageUrl: 'https://i.ibb.co/MN4d5G3/Fire-Shot-Capture-004-www-freecodecamp-org.png',
+      link: 'https://www.freecodecamp.org/certification/fccd9852319-c758-4155-a2ef-a8ffb02a5519/javascript-algorithms-and-data-structures',
+    },
+    // Adicione mais objetos de certificado conforme necessário
+  ];
+
+  return (
+    <div className="certificates-container mt-2">
+      <h2 className='column'>Meus Certificados</h2>
+      <Row className="justify-content-center">
+        {certificates.map((certificate, index) => (
+          <Col key={index} className="m-4" md={3}>
+            <h3 className='column'>{certificate.title}</h3>
+            <a href={certificate.link} target="_blank" rel="noopener noreferrer">
+              <img src={certificate.imageUrl} alt={certificate.title} className="certificate-image column" />
+            </a>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default CertificateList;
